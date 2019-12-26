064a14c3f6caad9737f15a8cac53496bd0a8d752
Horn, Benjamin Manfred <bhorn@mathematik.tu-darmstadt.de>
2019-12-26 16:56:59 +0100

    update default config

--- mqtt_config_default.py
+++ mqtt_config_default.py
@@ -1,4 +1,4 @@
-broker_ip = "localhost"
-broker_port = 1883
-client_username = "test_name"
-client_passwd = "test_passwd"
+broker_ip = "BROKER_IP_OR_LOCALHOST"
+broker_port = 1234
+client_username = "user_name_for_authentication"
+client_passwd = "password_for_authentication"
